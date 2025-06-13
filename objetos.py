dc459abe3a6dc6f4da8a402a7cfc5f05e11cfec7
CookieGalletita <javieravasquezmejias@gmail.com>
2025-06-13 01:18:58 -0400

    Update objetos.py

--- objetos.py
+++ objetos.py
@@ -1,16 +1,20 @@
 almacigo= 5
-
+almacigo_usado = []
 class Semilla: 
     def __init__ (self, nombre, cantidad):
         self.nombre = nombre #semilla nombre
         self.cantidad = cantidad
-        self.crecimiento= 5
+        
     
     def plantarSemilla (self):
         global almacigo
-        if self.cantidad >= 1 and almacigo > 1:
+        if self.cantidad >= 1 and almacigo >= 1:
             self.cantidad -=1
             almacigo -= 1
+
+            semilla_creciendo =SemillaPlantada(self.nombre)
+            almacigo_usado.append(semilla_creciendo)
+
             if self.cantidad ==1:
                 print (f"Has usado una {self.nombre} te queda {self.cantidad} semilla 🌱")
             elif self.cantidad == 0:
@@ -20,6 +24,17 @@ class Semilla:
         else:
             print(f"No hay suficiente {self.nombre}")
 
+        if almacigo_usado:
+            print(f"\nTienes {len(almacigo_usado)} semillas plantadas:")
+            for semilla in almacigo_usado:
+                print(f"- {semilla.nombre}, estado: {semilla.estado}")
+                print(f"Tienes {almacigo} almácigos libres.")
+        else:
+            print("\nNo hay semillas plantadas aún.")
+            print(f"Tienes {almacigo} almácigos libres.")
+    
+    
+
     def info (self):
         print(f"Tienes {self.cantidad} {self.nombre} 🌱")
         print(f"Tienes {almacigo} almacigos")
@@ -28,10 +43,23 @@ class Semilla:
 semillaRosa = Semilla("Semilla Rosa",5 )
 semillaTulipan= Semilla("Semilla Tulipan",5)
 
+class Flor:
+    def __init__ (self, nombre, cantidad):
+        self.nombre = nombre
+        self.cantidad = cantidad
+
+        
+
+   
+class SemillaPlantada:
+    def __init__(self, nombre):
+        self.nombre= nombre
+        self.estado= "Plantada" #creciendo lista
+
 semillaRosa.plantarSemilla()
 semillaRosa.info()
-
 semillaTulipan.plantarSemilla()
 semillaTulipan.info()
+
 
     
